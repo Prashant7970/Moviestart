170114b0042dae6e6bad50a890058097362e0cac
Amandeep32 <amandeep260500@gmail.com>
2023-03-05 10:48:16 +0530

    added something new

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let left_btn = document.getElementsByClassName('bi-chevron-left')[0];
 let right_btn = document.getElementsByClassName('bi-chevron-right')[0];
 let cards = document.getElementsByClassName('cards')[0];
+let search = document.getElementsByClassName('cards')[0];
 
 left_btn.addEventListener('click',()=>{
     cards.scrollLeft -= 140;
@@ -20,7 +21,42 @@ fetch(json_url).then(Response => Response.json())
         let {name, imdb, date, sposter, bposter, genre, url }= ele;
         let card = document.createElement('a');
         card.classList.add('card');
-        card.innerHTML = ''
+        card.href = url;
+        card.innerHTML = `
+        <img src="${sposter}" alt="${name}" class="poster">
+        <div class="rest_card">
+            <img src="${bposter}" alt="">
+            <div class="cont">
+                <h4>${name}</h4>
+                <div class="sub">
+                    <p>${genre}, ${date}</p>
+                    <h3> <span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</h3>
+                </div>
+            </div>
+        </div>
+        `
+        cards.appendChild(card);
+        
+         });
 
-    });
+         document.getElementById('title').innerText = data[0].name;
+         document.getElementById('gen').innerText = data[0].genre;
+         document.getElementById('date').innerText = data[0].date;
+         document.getElementById('rate').innerHTML = ` <span>IMDB</span><i class="bi bi-star-fill"></i>${data[0].imdb}`;
+         
+    //   search data load 
+    data.forEach(element => {
+        let {name, imdb, date, sposter, genre, url }= element;
+        let card = document.createElement('a');
+        card.classList.add('card');
+        card.href = url;
+        card.innerHTML = `
+        <img src="${sposter}" alt="">
+        <div class="cont">
+            <h3>${name}</h3>
+            <p>${genre},${date} , <span>IMDB</span><i class="bi bi-star-fill"></i>${imdb}</p>
+        </div>
+        `
+        search.appendChild(card);
+    })
 });
